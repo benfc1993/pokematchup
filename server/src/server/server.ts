ef42714c7ed4f815930c10d58710f0ba51f98c40
Ben <benfeldbergcollins@hotmail.co.uk>
2023-01-07 20:04:30 +0000

    add test

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -12,9 +12,15 @@ type Cache = {
 
 export let cache: Cache;
 export const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 // app.use(express.static(path.resolve(__dirname, '../../../app/build')));
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.write('LIVE');
+  res.end();
+});
 
 app.use('/team', teamRouter);
 app.use('/matchup', matchupsRouter);
@@ -23,7 +29,9 @@ app.use('/matchup', matchupsRouter);
 //   res.sendFile(path.resolve(__dirname, '../../../app/public/index.html'));
 // });
 
-app.listen(3001, async () => {
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, async () => {
   cache = JSON.parse((await readFile('data/cache.json')).toString());
-  console.log('Server listening on port 3001');
+  console.log(`Server listening on port ${PORT}`);
 });
